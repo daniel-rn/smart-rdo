c522e39c588384c73834a66b6255d312dad30587
daniel-rn <daniel.rn@live.com>
2020-06-21 21:26:20 -0300

    Inclui classe aplicationContext

---
+++ src/SmartRdo.Data/Context/AplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartRdo.Data.Context
+{
+    public class AplicationContext : DbContext
+    {
+        protected AplicationContext(DbContextOptions<AplicationContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
