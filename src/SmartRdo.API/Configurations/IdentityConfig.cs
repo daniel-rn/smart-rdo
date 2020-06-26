e0c559da4efae0207195396893c86820b352d33a
Daniel-Nascimento-EM <daniel.nascimento@escolarmanager.com.br>
2020-06-25 23:12:56 -0300

    Ajuste em identityConfig

--- src/SmartRdo.API/Configurations/IdentityConfig.cs
+++ src/SmartRdo.API/Configurations/IdentityConfig.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using SmartRdo.API.Data;
+using SmartRdo.API.Extensions;
 
 namespace SmartRdo.API.Configurations
 {
@@ -24,6 +28,31 @@ namespace SmartRdo.API.Configurations
             //    .AddEntityFrameworkStores<ApiDbContext>()
             //    //.AddErrorDescriber<IdentityMensagensPortugues>()
             //    .AddDefaultTokenProviders();
+
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
 
             return services;
         }
