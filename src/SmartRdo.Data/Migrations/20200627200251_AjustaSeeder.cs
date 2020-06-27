ca50a34e596963f845d3d7148081071daeb83027
daniel-rn <daniel.rn@live.com>
2020-06-27 17:24:08 -0300

    Inclui seeders Area e Responsavel

---
+++ src/SmartRdo.Data/Migrations/20200627200251_AjustaSeeder.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartRdo.Data.Migrations
+{
+    public partial class AjustaSeeder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: new Guid("bf31009f-1d9c-4c81-a4d5-2a5eb8611f95"));
+
+            migrationBuilder.DeleteData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: new Guid("ca52f3f4-03b6-403f-a143-45e90146ffd1"));
+
+            migrationBuilder.InsertData(
+                table: "Areas",
+                columns: new[] { "Id", "CodigoArea", "Nome" },
+                values: new object[,]
+                {
+                    { new Guid("c59c8d12-7ef0-47f7-9486-9c98b0589852"), "0001", "Galpao" },
+                    { new Guid("a92df7e5-8692-4859-80ec-9662e5524989"), "0002", "Patio" },
+                    { new Guid("880c6a2a-a4d9-41d4-b886-84b48c16d6fe"), "0003", "Refeitorioa" },
+                    { new Guid("362682de-e0a4-42e8-8e67-0ae9f720e724"), "0004", "Producao" },
+                    { new Guid("3b87e8f9-aad1-4864-b8fc-e9d545c044f2"), "0005", "Almoxarifado" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Clientes",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { new Guid("47019220-7be5-45db-b1c8-c7246d4c3bce"), "Cliente A" },
+                    { new Guid("865a1cfb-6972-48a6-9f64-fa18f59261cf"), "Cliente B" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responsaveis",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { new Guid("c59c8d12-7ef0-47f7-9486-9c98b0589852"), "Daniel Nascimento" },
+                    { new Guid("a92df7e5-8692-4859-80ec-9662e5524989"), "Murilo Seno" },
+                    { new Guid("880c6a2a-a4d9-41d4-b886-84b48c16d6fe"), "Gabriel Cotrim" },
+                    { new Guid("362682de-e0a4-42e8-8e67-0ae9f720e724"), "Gustavo Sousa" },
+                    { new Guid("3b87e8f9-aad1-4864-b8fc-e9d545c044f2"), "Ronaldo Ghesti" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Areas",
+                keyColumn: "Id",
+                keyValue: new Guid("362682de-e0a4-42e8-8e67-0ae9f720e724"));
+
+            migrationBuilder.DeleteData(
+                table: "Areas",
+                keyColumn: "Id",
+                keyValue: new Guid("3b87e8f9-aad1-4864-b8fc-e9d545c044f2"));
+
+            migrationBuilder.DeleteData(
+                table: "Areas",
+                keyColumn: "Id",
+                keyValue: new Guid("880c6a2a-a4d9-41d4-b886-84b48c16d6fe"));
+
+            migrationBuilder.DeleteData(
+                table: "Areas",
+                keyColumn: "Id",
+                keyValue: new Guid("a92df7e5-8692-4859-80ec-9662e5524989"));
+
+            migrationBuilder.DeleteData(
+                table: "Areas",
+                keyColumn: "Id",
+                keyValue: new Guid("c59c8d12-7ef0-47f7-9486-9c98b0589852"));
+
+            migrationBuilder.DeleteData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: new Guid("47019220-7be5-45db-b1c8-c7246d4c3bce"));
+
+            migrationBuilder.DeleteData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: new Guid("865a1cfb-6972-48a6-9f64-fa18f59261cf"));
+
+            migrationBuilder.DeleteData(
+                table: "Responsaveis",
+                keyColumn: "Id",
+                keyValue: new Guid("362682de-e0a4-42e8-8e67-0ae9f720e724"));
+
+            migrationBuilder.DeleteData(
+                table: "Responsaveis",
+                keyColumn: "Id",
+                keyValue: new Guid("3b87e8f9-aad1-4864-b8fc-e9d545c044f2"));
+
+            migrationBuilder.DeleteData(
+                table: "Responsaveis",
+                keyColumn: "Id",
+                keyValue: new Guid("880c6a2a-a4d9-41d4-b886-84b48c16d6fe"));
+
+            migrationBuilder.DeleteData(
+                table: "Responsaveis",
+                keyColumn: "Id",
+                keyValue: new Guid("a92df7e5-8692-4859-80ec-9662e5524989"));
+
+            migrationBuilder.DeleteData(
+                table: "Responsaveis",
+                keyColumn: "Id",
+                keyValue: new Guid("c59c8d12-7ef0-47f7-9486-9c98b0589852"));
+
+            migrationBuilder.InsertData(
+                table: "Clientes",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { new Guid("bf31009f-1d9c-4c81-a4d5-2a5eb8611f95"), "Cliente A" },
+                    { new Guid("ca52f3f4-03b6-403f-a143-45e90146ffd1"), "Cliente B" }
+                });
+        }
+    }
+}
---
+++ src/SmartRdo.Data/Seeder/Seeds/Seed003Area.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartRdo.Business.Models;
+using System;
+
+namespace SmartRdo.Data.Seeder.Seeds
+{
+    public class Seed003Area : ISeed
+    {
+        public void Executar(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Area>().HasData(
+            new Area { Id = Guid.Parse("c59c8d12-7ef0-47f7-9486-9c98b0589852"), Nome = "Galpao" , CodigoArea = "0001"},
+            new Area { Id = Guid.Parse("a92df7e5-8692-4859-80ec-9662e5524989"), Nome = "Patio", CodigoArea = "0002" },
+            new Area { Id = Guid.Parse("880c6a2a-a4d9-41d4-b886-84b48c16d6fe"), Nome = "Refeitorioa", CodigoArea = "0003" },
+            new Area { Id = Guid.Parse("362682de-e0a4-42e8-8e67-0ae9f720e724"), Nome = "Producao", CodigoArea = "0004" },
+            new Area { Id = Guid.Parse("3b87e8f9-aad1-4864-b8fc-e9d545c044f2"), Nome = "Almoxarifado", CodigoArea = "0005" }
+            );
+        }
+    }
+}
