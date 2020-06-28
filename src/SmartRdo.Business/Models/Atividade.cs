efa5ed1d74c3d42ed3ef28909918e00c4da5bfef
daniel-rn <daniel.rn@live.com>
2020-06-28 10:48:41 -0300

    Ajuste gerais

--- src/SmartRdo.Business/Models/Atividade.cs
+++ src/SmartRdo.Business/Models/Atividade.cs
@@ -1,43 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartRdo.Business.Models
 {
     public class Atividade : Entity
     {
+        /*
+         * Criar model no front e remover data anotations daqui
+         */
+
+        [Display(Name = "Código")]
         public string Codigo { get; set; }
-
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
-
         public DateTime Inicio { get; set; }
-
         public DateTime Fim { get; set; }
-
+        [Display(Name = "Previsão de início")]
         public DateTime InicioPrevisto { get; set; }
-
+        [Display(Name = "Previsão de término")]
         public DateTime FimPrevisto { get; set; }
-
+        [Display(Name = "Local de descarte")]
         public string LocalDescarte { get; set; }
-
+        [Display(Name = "Cliente")]
         public Guid ClienteId { get; set; }
-
         public Guid AreaId { get; set; }
-
         public Guid ResponsavelAreaId { get; set; }
-
         public AvaliacaoAtividade AvaliacaoAtividade { get; set; }
-
         public ICollection<AtividadeOperador> AtividadeOperador { get; set; }
-
         public ICollection<AtividadeRecurso> AtividadeRecurso { get; set; }
-
         public IEnumerable<AtividadeFotos> AtividadeFotos { get; set; }
 
         /* EF */
+        [Display(Name = "Cliente")]
         public Cliente Cliente { get; set; }
-
+        [Display(Name = "Área")]
         public Area Area { get; set; }
-
+        [Display(Name = "Responsável da área")]
         public ResponsavelArea ResponsavelArea { get; set; }
     }
 }
