308a2dc11df3433d8f53387907a529c7d3f9ed32
Ronaldo Ghesti <ronaldo.ghesti@outlook.com.br>
2020-08-24 20:12:49 -0300

    Ajuste nas telas de Atividades

--- src/SmartRdo.Business/Models/Area.cs
+++ src/SmartRdo.Business/Models/Area.cs
@@ -7,12 +7,14 @@ namespace SmartRdo.Business.Models
         public string Nome { get; set; }
 
         public string CodigoArea { get; set; }
-
+        
         public IEnumerable<ResponsaveisArea> ResponsaveisDaArea { get; set; }
 
         #region * EF Relations *
         
         public IEnumerable<Atividade> Atividades { get; set; }
         #endregion
+
+        public string InformacaoArea => $"{CodigoArea} - {Nome}";
     }
 }
