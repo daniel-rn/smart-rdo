6ab95d6bf03fa71fdac9325367b8dd5bf9a29874
Ronaldo Ghesti <ronaldo.ghesti@outlook.com.br>
2020-08-27 17:57:28 -0300

    Adiciona Migration de Equipamentos

---
+++ src/SmartRdo.Data/Migrations/20200827205332_Adiciona_Tabela_Equipamentos_E_Itens_Checklist.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartRdo.Data.Migrations
+{
+    public partial class Adiciona_Tabela_Equipamentos_E_Itens_Checklist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Equipamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensChecklistsEquipamento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IdEquipamento = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensChecklistsEquipamento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItensChecklistsEquipamento_Equipamentos_IdEquipamento",
+                        column: x => x.IdEquipamento,
+                        principalTable: "Equipamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensChecklistsEquipamento_IdEquipamento",
+                table: "ItensChecklistsEquipamento",
+                column: "IdEquipamento");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensChecklistsEquipamento");
+
+            migrationBuilder.DropTable(
+                name: "Equipamentos");
+        }
+    }
+}
